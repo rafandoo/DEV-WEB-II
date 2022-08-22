94fb213d1898a484ad2972e5a56ae916625d50de
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-08-22 10:57:36 -0300

    update atividade pintura cone

--- Laravel_aulas/routes/web.php
+++ Laravel_aulas/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HojeController;
 use App\Http\Controllers\TabController;
 use App\Http\Controllers\PiramideController;
+use App\Http\Controllers\ConeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,5 +33,6 @@ Route::get('/hoje', [HojeController::class, 'teste']);
 
 Route::get('/tab/{id?}/{ini?}/{fim?}', [TabController::class, 'tab']);
 
-Route::get('/piramide/{ab}/{h}/{tt?}', [PiramideController::class, 'piramide']);
+Route::get('/piramide/{ab?}/{h?}/{tt?}', [PiramideController::class, 'index'])->where(['ab' => '[0-9999.99]+', 'h' => '[0-9999.99]+', 'tt' => '[1-3]']);
 
+Route::get('/cone/{r?}/{h?}/{tt?}', [ConeController::class, 'index'])->where(['r' => '[0-9999.99]+', 'h' => '[0-9999.99]+', 'tt' => '[1-3]']);
